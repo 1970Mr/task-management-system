04385bbb1a21cf874b5ce633ed2779bdfefb31ef
1970Mr <mrepoch1970@gmail.com>
2024-07-29 14:39:30 +0430

    Add profile features

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 
 class LogoutController extends Controller
 {
-    public function __invoke()
+    public function __invoke(): JsonResponse
     {
         Auth::user()->tokens()->delete();
         return Response::json(['message' => 'Logged out successfully'], 200);
--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Profile\UpdateProfileRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
 
 class ProfileController extends Controller
 {
-    //
+    public function userProfile(Request $request): JsonResponse
+    {
+        return Response::json($request->user());
+    }
+
+    public function updateProfile(UpdateProfileRequest $request): JsonResponse
+    {
+        $user = $request->user();
+        $data = $this->getValidData($request);
+        $user->update($data);
+        return Response::json($user);
+    }
+
+    private function getValidData(UpdateProfileRequest $request): array
+    {
+        $data = $request->validated();
+        if ($request->has('password')) {
+            $data['password'] = Hash::make($request->password);
+        }
+        return $data;
+    }
 }
