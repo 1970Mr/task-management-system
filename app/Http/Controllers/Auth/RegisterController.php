6cdb575b6e4a2adeae2c77edd27f2f039de70087
1970Mr <mrepoch1970@gmail.com>
2024-07-29 14:11:29 +0430

    Add sanctum pacakge and develop RegisterController and RegisterRequest

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    public function __invoke(RegisterRequest $request): JsonResponse
+    {
+        /** @var User $user */
+        $user = User::query()->create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer'], 201);
+    }
 }
