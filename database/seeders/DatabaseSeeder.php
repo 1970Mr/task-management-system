d3418015997c23f6834a754a30e09e6a1338a94c
1970Mr <mrepoch1970@gmail.com>
2024-07-30 10:30:28 +0430

    Add seeder for create the first admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->admin()->create([
+            'name' => config('app.admin.name'),
+            'email' => config('app.admin.email'),
+            'password' => config('app.admin.password'),
         ]);
     }
 }
